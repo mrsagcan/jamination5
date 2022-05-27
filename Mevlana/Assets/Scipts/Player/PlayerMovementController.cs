e7567273df632e8e02313bb31ba45e147bdaeaad
Denizhan <65414809+Elmarath@users.noreply.github.com>
2022-05-28 01:03:37 +0300

    Movement Basic added

---
+++ Mevlana/Assets/Scipts/Player/PlayerMovementController.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementController : MonoBehaviour
+{
+    [SerializeField] private float angularSpeed = 10f;
+    [SerializeField] private float forwardSpeed = 100f;
+    private Rigidbody playerRb;
+    private Vector3 playerCurrentAngularVeloctiy;
+    private bool isTurning;
+    private bool isTurningRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        playerCurrentAngularVeloctiy = Vector3.zero;
+        isTurning = true;
+        isTurningRight = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            isTurning = false;
+            MoveForward();
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            isTurning = true;
+            playerRb.velocity = Vector3.zero;
+            isTurningRight = !isTurningRight;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (isTurning)
+        {
+            RotatePlayer(isTurningRight);
+        }
+        else
+        {
+            playerRb.angularVelocity = Vector3.zero;
+        }
+    }
+
+    void RotatePlayer(bool isTurningRight)
+    {
+        if (isTurningRight)
+        {
+            playerRb.angularVelocity = new Vector3(0f, angularSpeed, 0f);
+        }
+        else
+        {
+            playerRb.angularVelocity = new Vector3(0f, -angularSpeed, 0f);
+        }
+    }
+
+    void MoveForward()
+    {
+        Vector3 forwardVector = transform.right;
+        playerRb.AddForce(forwardVector * forwardSpeed, ForceMode.Acceleration);
+    }
+}
